d6a5634fc441adcced6afee1365d3804a4118c93
Vladimir Izmalkov <vladimir@vaticle.com>
2024-01-24 09:50:42 +0000

    Update explanation example

--- explain_example.py
+++ explain_example.py
@@ -1,17 +1,15 @@
-from typedb.client import TypeDB, SessionType, TransactionType, TypeDBOptions
+from typedb.driver import TypeDB, SessionType, TransactionType, TypeDBOptions
 
-with TypeDB.core_client("0.0.0.0:1729") as client:  # Connect to TypeDB server
+with TypeDB.core_driver("127.0.0.1:1729") as client:  # Connect to TypeDB server
     with client.session("iam", SessionType.DATA) as session:
         print("\nRequest #1: Explain query — Files that Kevin Morrison has view access to (with explanation)")
-        typedb_options = TypeDBOptions.core()  # Initialising a new set of options
-        typedb_options.infer = True  # Enabling inference in this new set of options
-        typedb_options.explain = True
+        typedb_options = TypeDBOptions(infer=True, explain=True)  # Initialising a new set of options
         with session.transaction(TransactionType.READ, typedb_options) as transaction:
             typeql_read_query = "match $p isa person, has full-name $p-fname; $o isa object, has path $o-path;" \
-                                "$a isa action, has action-name 'view_file'; $ac(accessed-object: $o, valid-action: $a) isa access;" \
-                                "$pe(permitted-subject: $p, permitted-access: $ac) isa permission; $p-fname = 'Kevin Morrison';" \
+                                "$a isa action, has name 'view_file'; $ac(object: $o, action: $a) isa access;" \
+                                "$pe(subject: $p, access: $ac) isa permission; $p-fname = 'Kevin Morrison';" \
                                 "get $o-path; sort $o-path asc;"
-            iterator = transaction.query().match(typeql_read_query)
+            iterator = transaction.query.get(typeql_read_query)
             i = 0
             for item in iterator:  # Iterating through results
                 i += 1
@@ -19,7 +17,7 @@ with TypeDB.core_client("0.0.0.0:1729") as client:  # Connect to TypeDB server
                 e = 0
                 for explainable in explainable_relations:
                     e += 1
-                    explain_iterator = transaction.query().explain(explainable_relations[explainable])
+                    explain_iterator = transaction.query.explain(explainable_relations[explainable])
                     ex = 0
                     for explanation in explain_iterator:
                         ex += 1
@@ -30,8 +28,11 @@ with TypeDB.core_client("0.0.0.0:1729") as client:  # Connect to TypeDB server
                         print("Explainable part of query:", explainable_relations[explainable].conjunction())
                         print("Explanation #:", ex)
 
-                        print("\nRule: ", explanation.rule().get_label())
+                        print("\nRule: ", explanation.rule().label)
                         print("Condition: ", explanation.condition())
                         print("Conclusion: ", explanation.conclusion())
-                        print("Variables used in explanation: ", explanation.variable_mapping())
+                        # print("Variables used in explanation: ", explanation.query_variables())
+                        print("Variable mapping: ")
+                        for var in explanation.query_variables():
+                            print(f"  Query variable {var} maps to the rule variable {explanation.query_variable_mapping(var)}")
                         print("----------------------------------------------------------")
